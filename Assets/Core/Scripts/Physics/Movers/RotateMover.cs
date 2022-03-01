4af5237d45f0749172ffe8115bd7e3ea20926a5c
Pierre ARNAUDET <loufhok@gmail.com>
2022-03-01 11:11:45 +0100

    Script correction

--- Assets/Core/Scripts/Physics/Movers/RotateMover.cs
+++ Assets/Core/Scripts/Physics/Movers/RotateMover.cs
@@ -2,17 +2,59 @@ namespace PierreARNAUDET.Core.Movers
 {
     using UnityEngine;
 
+    using PierreARNAUDET.Core.Attributes;
+
     public class RotateMover : UMover
     {
-        [Header("Data")]
+        [Data]
         [SerializeField] Transform transformToMove;
         public Transform TransformToMove { get => transformToMove; set => transformToMove = value; }
-        [SerializeField] Vector3 rotationToApply;
+
+        [Settings]
+        [SerializeField] bool automaticFixedUpdate;
+        [SerializeField] bool automaticUpdate;
+        [SerializeField] bool randomRotation;
+        [SerializeField, ConditionalHide("randomRotation", true, true)] Vector3 rotationToApply;
         public Vector3 RotationToApply { get => rotationToApply; set => rotationToApply = value; }
+        [SerializeField, ConditionalHide("randomRotation", true, false)] float maxRandomRange;
+
+        Vector3 rotation;
+
+        private void Awake()
+        {
+            if (randomRotation)
+            {
+                rotation = new Vector3(
+                    Random.Range(-maxRandomRange, maxRandomRange),
+                    Random.Range(-maxRandomRange, maxRandomRange),
+                    Random.Range(-maxRandomRange, maxRandomRange)
+                    );
+            }
+            else
+            {
+                rotation = rotationToApply;
+            }
+        }
+
+        private void Update()
+        {
+            if (automaticUpdate)
+            {
+                Rotate();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (automaticFixedUpdate)
+            {
+                Rotate();
+            }
+        }
 
         public void Rotate()
         {
-            transformToMove.Rotate(rotationToApply);
+            transformToMove.Rotate(rotation);
         }
     }
 }
