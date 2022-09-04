d5c3da696f07d9ec4d2731ab167833e5081f4e88
Pierre ARNAUDET <loufhok@gmail.com>
2022-09-04 18:12:03 +0200

    Added Vector3MultiplicationOperation

---
+++ Assets/Core/Scripts/Operations/Vector3/Vector3MultiplicationOperation.cs
@@ -0,0 +1,28 @@
+﻿namespace PierreARNAUDET.Core.Operations
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    using PierreARNAUDET.Core.Events;
+
+    public class Vector3MultiplicationOperation : UOperation<Vector3>
+    {
+        [Header("Data")]
+        [SerializeField] Vector3 leftOperand;
+        public override Vector3 LeftOperand { get => leftOperand; set => leftOperand = value; }
+        [SerializeField] Vector3 rightOperand;
+        public override Vector3 RightOperand { get => rightOperand; set => rightOperand = value; }
+
+        [Header("Events")]
+        [SerializeField] Vector3Event result;
+        protected override UnityEvent<Vector3> Result => result;
+
+        protected override Vector3 OperateObjects(Vector3 leftOperand, Vector3 rightOperand)
+        {
+            var x = leftOperand.x * rightOperand.x;
+            var y = leftOperand.y * rightOperand.y;
+            var z = leftOperand.z * rightOperand.z;
+            return new Vector3(x, y, z);
+        }
+    }
+}
